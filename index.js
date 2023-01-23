7f4345c7fae4a9125021a17441eb44b1487e83bc
mariblan <99274158+mariblan@users.noreply.github.com>
2023-01-23 11:54:42 +0100

    step2

--- index.js
+++ index.js
@@ -2,3 +2,16 @@ function hello() {
   console.log('hello');
 }
 hello();
+
+const fs = require('fs');
+
+function writeFile(file, data) {
+  fs.writeFile(file, data, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('file created');
+    }
+  });
+}
+writeFile('test.html', '<h1>hello</h1>');
